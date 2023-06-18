a71b6585409d2860cc5a7e563bb8620ecb88fd6d
Nishant-modi <mnishant0123@gmail.com>
2023-06-19 01:11:19 +0530

    idk whats up

--- src/worlds/Starter.tsx
+++ src/worlds/Starter.tsx
@@ -1,16 +1,27 @@
-import { Spinning, Floating, StandardReality, Model } from "spacesvr";
+import { Spinning, Floating, StandardReality, Model, Fog, HDRI, EnvironmentProps} from "spacesvr";
 import TransparentFloor from "ideas/TransparentFloor";
 import CloudySky from "ideas/CloudySky";
-import {Sky} from "@react-three/drei";
+import React, {useRef} from 'react';
+import THREE from 'three';
+import { useFrame, useThree, useLoader, Canvas, createPortal as createPortal$1, extend } from '@react-three/fiber';
+//import { Canvas } from 'react-three-fiber';
+import {Box, Sky, shaderMaterial} from "@react-three/drei";
+
+//import glsl from 'glslify';
+
+
 export default function Starter() {
   return (
     <StandardReality>
-      <ambientLight intensity = {0} />
+      <ambientLight intensity = {1} />
       <Model scale = {0.007} rotation-x = {-Math.PI/2} position-y = {0.01} src = "./vrShowcaseGlassFixed.glb"/>
       <Model scale = {0.01} position-y = {0.7} src = "./product.glb"/>
-      <Model scale = {1} position-y = {0.7} src = "./shader test cycles.glb"/>
-      <TransparentFloor opacity={2} />
-      <Sky/>
+      
+      <pointLight intensity = {1} position={[10, 10, 11]} />
+      <Fog color = "black" near = {0.1} far = {10}/>
+      <HDRI src = "./Skyhdr1.hdr"  disableBackground = {false} disableEnvironment = {true}/>
+      <TransparentFloor opacity={0.1} />
+      
     </StandardReality>
   );
 }
