f53cfe4787819abbb075c349d92c3356709ae3c6
Nishant-modi <mnishant0123@gmail.com>
2023-07-28 16:28:41 +0530

    Postprocessing Look Change

---
+++ src/ideas/Bloom.tsx
@@ -0,0 +1,57 @@
+import { VisualEffect } from "myvr/mymain.js";
+import { UnrealBloomPass, DotScreenPass, HalftonePass, HalftoneShader} from "three-stdlib";
+import { extend, ReactThreeFiber } from "@react-three/fiber";
+
+import { useState } from "react";
+import { Vector2 } from "three";
+
+extend({ UnrealBloomPass, DotScreenPass, HalftonePass, HalftoneShader });
+
+
+const params = {
+    shape: 1,
+    radius: 5,
+    rotateR: Math.PI / 12,
+    rotateB: Math.PI / 12 * 2,
+    rotateG: Math.PI / 12 * 3,
+    scatter: 0.3,
+    blending: 0.5,
+    blendingMode: 1,
+    greyscale: 0,
+    disable: 0
+};
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      unrealBloomPass: ReactThreeFiber.Node<
+        UnrealBloomPass,
+        typeof UnrealBloomPass
+      >;
+      dotScreenPass: ReactThreeFiber.Node<
+        DotScreenPass,
+        typeof DotScreenPass
+      >;
+      halftonePass: ReactThreeFiber.Node<
+        HalftonePass,
+        typeof HalftonePass
+      >;
+    }
+  }
+}
+
+
+
+export default function Bloom() {
+  const [res] = useState(() => new Vector2(1024, 1024));
+
+  return (
+    <VisualEffect index={1}>
+      
+      <unrealBloomPass args={[res, 2, 1, 0.95]} />
+      <dotScreenPass args={[res, 1, 2]} />
+      {/*<halftonePass args = {[1,1,params]}/>*/}
+    </VisualEffect>
+  );
+}
--- src/worlds/Starter.tsx
+++ src/worlds/Starter.tsx
@@ -1,16 +1,23 @@
-import { Spinning, Floating, Model, Fog, HDRI, EnvironmentProps, Collidable, PlayerProps, Player, useEnvironment, useKeyboardLayout, Idea, MenuItem, Video} from "spacesvr";
-import {StandardReality, Interactable} from "myvr/mymain.js";
+import { Spinning, Floating,Model, Fog, HDRI, EnvironmentProps, PlayerProps, Player, useEnvironment, useKeyboardLayout, Idea, MenuItem, Video, Image} from "spacesvr";
+import {StandardReality, Interactable, VisualEffect, Collidable} from "myvr/mymain.js";
 import TransparentFloor from "ideas/TransparentFloor";
 import CloudySky from "ideas/CloudySky";
-import React, {Suspense, useRef} from 'react';
-import THREE, { IcosahedronGeometry } from 'three';
-import { EffectComposer, DotScreen } from "@react-three/postprocessing";
-import { BlendFunction } from "postprocessing";
-import { useFrame, useThree, useLoader, Canvas, createPortal as createPortal$1, extend } from '@react-three/fiber';
+import React, {Suspense, useRef, useState} from 'react';
+import THREE, { IcosahedronGeometry, Vector2} from 'three';
+import { EffectComposer, DotScreen, LensFlare} from "@react-three/postprocessing";
+import { BlendFunction} from "postprocessing";
+//import {DotScreenPass} from "three/examples/jsm/postprocessing/DotScreenPass";
+import { UnrealBloomPass, DotScreenPass } from "three-stdlib";
+//import {UnrealBloomPass} from "three/examples/jsm/postprocessing/UnrealBloomPass";
+import { useFrame, useThree, useLoader, Canvas, createPortal as createPortal$1, extend, ReactThreeFiber } from '@react-three/fiber';
 //import { Canvas } from 'react-three-fiber';
-import {Box, Icosahedron, Sky, shaderMaterial} from "@react-three/drei";
+import {Box, Center, Icosahedron, Sky, shaderMaterial} from "@react-three/drei";
+import Bloom from "../ideas/Bloom";
 
 //import glsl from 'glslify';
+
+
+
 
 function Effects() {
   return (
@@ -30,8 +37,32 @@ function Effects() {
 export default function Starter() {
   return (
     
-    <StandardReality playerProps= {{pos : [-45,1,65] , speed : 6}} environmentProps={{pauseMenu : null}}>
-      {/*<ambientLight intensity = {0} />
+    <StandardReality playerProps= {{pos : [-45,1,65] , speed : 6}} environmentProps={{pauseMenu : null}} shadowMap>
+      <Bloom/>
+      <ambientLight intensity = {0.3} />
+      <pointLight intensity = {1} position={[10, 10, 11]} />
+      
+      <directionalLight intensity = {2} castShadow position={[1, 1, 1]} shadow-mapSize={[1024, 1024]}>
+      <orthographicCamera attach="shadow-camera" args={[-10, 10, 10, -10]} />
+  </directionalLight>
+
+  
+      
+
+      {/*
+      <EffectComposer>
+      <DotScreen
+        blendFunction={BlendFunction.NORMAL}
+        angle={Math.PI * 0.5}
+        scale={5}
+        opacity = {0.2}
+      />
+    </EffectComposer>
+
+    <VisualEffect index={0}>
+    <UnrealBloomPass args={[new Vector2(256, 256), 0.1, 0.01, 0.95]} />
+    </VisualEffect>
+      <ambientLight intensity = {0} />
       {/*<Model scale = {0.007} rotation-x = {-Math.PI/2} position-y = {0.01} src = "./vrShowcaseGlassFixed.glb"/>
       <Model scale = {0.01} position-y = {0.7} src = "./product.glb"/>
       
@@ -51,15 +82,17 @@ export default function Starter() {
       <Model scale = {0.5} position-y = {-1} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Final2.glb"/>*/}
 
       <Collidable triLimit={10000} enabled={true} hideCollisionMeshes={false}>
-      <Model scale = {0.5} position-y = {-1.7} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Beam.glb"/>
-      <Model scale = {0.5} position-y = {-1} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Islands.glb"/>
-      <Model scale = {0.5} position-y = {-1} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Bridges.glb"/>
+      <Model scale = {0.5} position-y = {-1.7} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/BeamLines.glb"/>
+      <Model scale = {0.5} position-y = {-1.7} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/MainBeam.glb"/>
+      <Model receiveShadow scale = {0.5} position-y = {-1} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Islands.glb"/>
+      <Model castShadow receiveShadow scale = {0.5} position-y = {-1} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Bridges.glb"/>
       <Model scale = {0.5} position-y = {-1.5} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Machine_Base.glb"/>
-      <Model scale = {0.5} position-y = {-2} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Machine.glb"/>
-      <Model scale = {0.5} position-y = {-1.7} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Dish.glb"/>
-      <Model scale = {0.5} position-y = {-1} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Hall.glb"/>
+      <Model castShadow scale = {0.5} position-y = {-2} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Machine.glb"/>
+      <Model castShadow scale = {0.5} position-y = {-1.7} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Dish.glb"/>
+      <Model castShadow scale = {0.5} position-y = {-1} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Hall.glb"/>
       <Model scale = {0.5} position-y = {-1} position-x = {-100} rotation-y={(Math.PI/2)+0.153} src = "./Models/Banners.glb"/>
       </Collidable >
+      
       
 
       <Interactable onClick={() => window.open("https://jmangoes.itch.io/mirrors",'_blank')}>
@@ -81,7 +114,9 @@ export default function Starter() {
       </Interactable>
 
       <Interactable onClick={() => window.open("https://jmangoes.itch.io/mirrors",'_blank')}>
-      <Video src="./Banners/Mirrors_Banner.mp4" size={12.1} position = {[-90.25, 6, -15.3]} rotation-y={0.153} framed frameWidth={1} /> {/*right first banner*/}
+      <Video src="./Banners/Mirrors_Banner2.gif" size={12.1} position = {[-90.25, 6, -15.3]} rotation-y={0.153} framed frameWidth={1} /> {/*right first banner*/}
+
+      <Image src = "./Banners/Mirrors_Banner2.gif" size={12.1} position = {[-90.25, 6, -15.3]} rotation-y={0.153}/>
       <Model scale = {0.5} position = {[-101, -1 , 0]} rotation-y={(Math.PI/2)+0.153} src = "./Models/Buttons/Button_RF.glb"/>
       </Interactable>
 
@@ -96,12 +131,12 @@ export default function Starter() {
       </Interactable>
 
       
-      <Effects/>
-
+      
 
       {/*<Fog color = "#2C1A10" near = {0.1} far = {80}/>*/}
       <HDRI src = "./Skyhdr4.hdr" rotation-z = {Math.PI/2} rotation-y = {Math.PI} /> 
       {/*<TransparentFloor opacity={0.1} />*/}
+      
       
     </StandardReality>
   );
